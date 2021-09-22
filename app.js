873c242e25992bfd2d8203e9bede7ec9bb8bf47f
sudarshanchakra <91173966+sudarshanchakra@users.noreply.github.com>
2021-09-22 10:19:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+/*console
+console.log("Ayush");
+console.warn("Ayush");
+console.error("ayush");
+*/
+//alert
+//alert("Ayush");
+/*var name = prompt("Enter Your Name");
+var gdname = "Good Morning " + name;
+
+console.log(gdname);
+*/
+/*
+//variables
+var string = "This is a String";
+var int = 10;
+var float = 10.9;
+var object = {ayush :10,mama:10,kanha:90};
+var array = ["ayush","aadyasha","arush"];
+console.log(array);
+*/
+/*
+var age = prompt("Enter age Name");
+if(age >= 18){
+    console.log("You are adult")
+}else if(age > 13 && age < 18){
+    console.log("You are a Teenager");
+}else{
+    console.log("You are a kid")
+}
+*/
