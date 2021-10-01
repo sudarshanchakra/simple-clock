00448ae3c8cbef934d4132fefe8bcdb284d3e415
Ayush Ranjan Rout Rout <57044707+AyushRanjanRout@users.noreply.github.com>
2021-10-01 08:51:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-/*console
-console.log("Ayush");
-console.warn("Ayush");
-console.error("ayush");
-*/
-//alert
-//alert("Ayush");
-/*var name = prompt("Enter Your Name");
-var gdname = "Good Morning " + name;
-
-console.log(gdname);
-*/
-/*
-//variables
-var string = "This is a String";
-var int = 10;
-var float = 10.9;
-var object = {ayush :10,mama:10,kanha:90};
-var array = ["ayush","aadyasha","arush"];
-console.log(array);
-*/
-/*
-var age = prompt("Enter age Name");
-if(age >= 18){
-    console.log("You are adult")
-}else if(age > 13 && age < 18){
-    console.log("You are a Teenager");
-}else{
-    console.log("You are a kid")
-}
-*/
+console.log("Welcome To Simple Clock");
+let a;
+let time;
+setInterval(() => {
+                a = new Date();
+            time = a.getHours() + ": " + a.getMinutes() + ": " + a.getSeconds();
+            document.getElementById('time').innerHTML = time;
+            },1000);
+            
+        
